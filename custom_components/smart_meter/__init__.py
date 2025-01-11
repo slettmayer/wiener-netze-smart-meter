42a57c3f0bacda9eb842b4beebc35060330af41b
Fabian Guschlbauer <fabian.guschlbauer.ext@porscheinformatik.com>
2025-01-11 17:02:53 +0100

    fix call of async_forward_entry_setup for integration

--- custom_components/smart_meter/__init__.py
+++ custom_components/smart_meter/__init__.py
@@ -6,20 +6,20 @@ from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass: HomeAssistant, config: {}):
+async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the smart_meter component."""
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up smart_meter from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    _LOGGER.debug(f"Setting up Smart Meter integration for {entry.title}")
+
+    # Use async_forward_entry_setups instead of the deprecated async_forward_entry_setup
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-
     return True
--- custom_components/smart_meter/config_flow.py
+++ custom_components/smart_meter/config_flow.py
@@ -7,16 +7,17 @@ class SmartMeterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for smart_meter."""
     VERSION = 1
 
-    async def async_step_user(self, info):
-
+    async def async_step_user(self, info=None):
         if info is not None:
             return self.async_create_entry(title="Smart Meter", data=info)
 
-        return self.async_show_form(step_id="user", data_schema=vol.Schema(
-            {
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
                 vol.Required("GP", default="120305XXXX"): str,
                 vol.Required("DEVICE", default="AT001000000000000000100001519XXXX"): str,
                 vol.Required("TOKEN", default="Visit https://smartmeter-web.wienernetze.at/, copy 'KEYCLOAK_IDENTITY' cookie from developer tools"): str,
                 vol.Required("INTERVAL", default=60): int,
                 vol.Required("15MIN_ENABLED", default=False): bool
-            }))
+            })
+        )
